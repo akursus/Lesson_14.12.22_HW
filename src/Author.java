fae7bcd355a97116583c2a68f1e40b75a7dbcb04
akursus <ak_ursus@list.ru>
2022-12-24 18:34:59 +0300

    Task

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,45 @@
+import java.util.Objects;
+
 public class Author {
+    private String nameAuthor;
+    private String surnameAuthor;
+
+    public Author(String nameAuthor, String surnameAuthor) {
+        this.nameAuthor = nameAuthor;
+        this.surnameAuthor = surnameAuthor;
+    }
+
+    public String getNameAuthor() {
+        return nameAuthor;
+    }
+
+    public void setNameAuthor(String nameAuthor) {
+        this.nameAuthor = nameAuthor;
+    }
+
+    public String getSurnameAuthor() {
+        return surnameAuthor;
+    }
+
+    public void setSurnameAuthor(String surnameAuthor) {
+        this.surnameAuthor = surnameAuthor;
+    }
+
+    @Override
+    public String toString() {
+        return nameAuthor + " " + surnameAuthor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return nameAuthor.equals(author.nameAuthor) && surnameAuthor.equals(author.surnameAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameAuthor, surnameAuthor);
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,52 @@
+import java.util.Objects;
+
 public class Book {
+    private String nameBook;
+    private final Author author;
+    private int publicationYear;
+
+    public Book(String nameBook, Author author, int publicationYear) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public void setNameBook(String nameBook) {
+        this.nameBook = nameBook;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " +nameBook + ", Автор: " + author + ", Год издания: " + publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && nameBook.equals(book.nameBook) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, author, publicationYear);
+    }
+
 }
