b0af2415390e39b4b84919b639151881384cb3f4
akursus <ak_ursus@list.ru>
2022-12-24 18:34:59 +0300

    Task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Михаил", "Булгаков");
+        Author author2 = new Author("Антон", "Чехов");
+        System.out.println(author1);
+        System.out.println(author2);
+        Book book1 = new Book("Собачье сердце", author1, 1925);
+        Book book2 = new Book("Пари", author2, 1889);
+        System.out.println(book1);
+        System.out.println(book2);
 
     }
 }
